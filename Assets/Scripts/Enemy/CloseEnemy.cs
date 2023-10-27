1207914378cd5ac478ad8e6e744f2c239c4b84f8
Chengzi-creator <1041379828@qq.com>
2023-10-28 01:23:25 +0800

    28号1:22
    实现了第一个敌人的左右移动，撞墙检测以及停顿间隔

--- Assets/Scripts/Enemy/CloseEnemy.cs
+++ Assets/Scripts/Enemy/CloseEnemy.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.ComponentModel.Design;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class CloseEnemy : MonoBehaviour
 {
     private Rigidbody2D rb;
     //protected Animator anim;
     private CapsuleCollider2D coll;
+    private PhysicCheck physicCheck;
     
     [Header("基本参数")] 
     public float normalSpeed;//基础速度
@@ -17,16 +18,11 @@ public class Enemy : MonoBehaviour
     public Vector3 faceDir;//面朝对象
     public Transform attacker;//被攻击的对象
     public float hurtForce;
-    [Header("检测参数")] 
-    public Vector2 leftOffset;//左方检测
-    public bool manual;//可以自动进行检测bool值
-    public Vector2 rightOffset;//右方检测
-    public float checkRaduis;//检测的基础范围
-    public LayerMask groundLayer;
+    
     
     [Header("状态")] 
-    public bool touchLeftWall;//移动怪物是否触碰左墙
-    public bool touchRightWall; //移动怪物是否触碰右墙
+    // public bool touchLeftWall;//移动怪物是否触碰左墙
+    // public bool touchRightWall; //移动怪物是否触碰右墙
     public bool isHurt;
     public bool isDead;
     
@@ -40,16 +36,10 @@ public class Enemy : MonoBehaviour
         isHurt = false;
         isDead = false;
         rb = GetComponent<Rigidbody2D>();
+        physicCheck = GetComponent<PhysicCheck>();
         //anim = GetComponent<Animator>();
         coll = GetComponent<CapsuleCollider2D>();//获取组件
         currentSpeed = normalSpeed;
-        if (!manual)
-        {   
-            //判断下左右的检测范围
-            rightOffset = new Vector2((coll.bounds.size.x + coll.offset.x)/2,coll.bounds.size.y/2);
-            leftOffset = new Vector2(-(coll.bounds.size.x + coll.offset.x)/2,coll.bounds.size.y/2);
-        }
-
         waitTimeCounter = waitTime;
     }
 
@@ -60,8 +50,15 @@ public class Enemy : MonoBehaviour
     
     private void Update()
     {   
-        Debug.Log("为什么不运行");
-        faceDir = new Vector3(transform.localScale.x,0,0);//获取敌人的面朝方式
+        faceDir = new Vector3(-transform.localScale.x,0,0);//获取敌人的面朝方式
+        
+        if ((physicCheck.touchLeftWall && faceDir.x < 0)|| (physicCheck.touchRightWall && faceDir.x > 0))
+        {      
+            Debug.Log("撞墙");
+            wait = true;
+            //anim.SetBool("Move",false);
+        }
+        TimeCounter();
     }
 
     private void FixedUpdate()
@@ -70,38 +67,15 @@ public class Enemy : MonoBehaviour
         if (!isHurt && !isDead)
         {
             Move();
-            Check();
         }
     }
 
     public virtual void Move()//希望可以复写
     {   
         Debug.Log("移动");
-        rb.velocity = new Vector2(currentSpeed *  transform.localScale.x, rb.velocity.y);//设置了敌人的移动
+        rb.velocity = new Vector2(currentSpeed *  faceDir.x, rb.velocity.y);//设置了敌人的移动
     }
-
-    public void Check()
-    {   
-        
-        
-        //墙体判断,左右墙
-        touchLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, checkRaduis, groundLayer);
-        touchRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, checkRaduis, groundLayer);
-        if ((touchLeftWall && faceDir.x > 0)|| (touchRightWall && faceDir.x < 0) )
-        {   
-            Debug.Log("撞墙");
-            wait = true;
-            //anim.SetBool("Move",false);
-        }
-        TimeCounter();
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, checkRaduis);
-        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, checkRaduis);
-    }
-
+    
     public void TimeCounter()
     {   
         if (wait)
@@ -112,7 +86,7 @@ public class Enemy : MonoBehaviour
                 Debug.Log("进行改变");
                 wait = false;
                 waitTimeCounter = waitTime;
-                transform.localScale = new Vector3(-faceDir.x, 1, 1);
+                transform.localScale = new Vector3(faceDir.x, 1, 1);
             }
         }
     }
@@ -147,7 +121,7 @@ public class Enemy : MonoBehaviour
 
     public void OnDie()
     {
-        gameObject.layer = 2;//这里的第二个图层之后设置为忽略的图层，这里面的物体不会与角色产生碰撞
+        gameObject.layer = 5;//这里的第五个图层之后设置为忽略的图层，这里面的物体不会与角色产生碰撞
         //anim.SetBool("Dead",true);
         isDead = true;
     }
---
+++ Assets/Scripts/General/Character.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [Header("基础属性")] 
+    public float maxHealth;
+
+    public float currentHealth;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
