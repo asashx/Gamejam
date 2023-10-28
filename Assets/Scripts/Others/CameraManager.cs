b844892c87ae756abc0c337c2ea959db75321ac5
shi <815599298@qq.com>
2023-10-28 13:30:21 +0800

    摄像机跟随

---
+++ Assets/Scripts/Others/CameraManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 1f;
+
+    private void LateUpdate() {
+        if (target != null) {
+            if(transform.position != target.position) {
+                Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+                transform.position = Vector3.Lerp(transform.position, targetPos, smoothSpeed);
+            }
+        }
+    }
+}
